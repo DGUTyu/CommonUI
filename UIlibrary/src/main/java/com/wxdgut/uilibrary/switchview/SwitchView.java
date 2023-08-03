b1043e779c2147a22fca5570af60b44bf8cb73e7
luyao <hello_luyao@163.com>
2023-08-03 17:10:25 +0800

    添加自定义开关SwitchView

---
+++ UIlibrary/src/main/java/com/wxdgut/uilibrary/switchview/SwitchView.java
@@ -0,0 +1,161 @@
+package com.wxdgut.uilibrary.switchview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.wxdgut.uilibrary.R;
+
+public class SwitchView extends View {
+    private int circlePadding;
+    private int switchWidth;
+    private int switchHeight;
+    private int borderWidth;
+    private int onColorBg;
+    private int offColorBg;
+    private int onColor;
+    private int offColor;
+    private int borderColor;
+
+    private boolean isChecked;
+    private ClickListener listener;
+
+    private Paint switchPaint;
+    private Paint borderPaint;
+
+    private int radius;
+    private Path clipPath;
+    private Path borderPath;
+    private RectF borderRectF;
+
+    public SwitchView(Context context) {
+        super(context);
+        init(null);
+        initListener();
+    }
+
+    private void initListener() {
+        setOnClickListener(view -> {
+            isChecked = !isChecked;
+            invalidate();
+            if (listener != null) {
+                listener.onSwitchStateChanged(isChecked);
+            }
+        });
+    }
+
+    public SwitchView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+        initListener();
+    }
+
+    public SwitchView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs);
+        initListener();
+    }
+
+    private void init(AttributeSet attrs) {
+        // 获取其他属性
+        TypedArray ta = getContext().obtainStyledAttributes(attrs, R.styleable.SwitchView);
+        circlePadding = ta.getDimensionPixelSize(R.styleable.SwitchView_circlePadding, 0);
+        switchWidth = ta.getDimensionPixelSize(R.styleable.SwitchView_switchWidth, DensityUtil.dpToPx(60));
+        switchHeight = ta.getDimensionPixelSize(R.styleable.SwitchView_switchHeight, DensityUtil.dpToPx(30));
+        borderWidth = ta.getDimensionPixelSize(R.styleable.SwitchView_borderWidth, DensityUtil.dpToPx(2));
+        // 使用透明度属性来设置 onColorBg 和 offColorBg 的透明度
+        float onAlpha = ta.getFloat(R.styleable.SwitchView_onAlpha, 0.2f);
+        float offAlpha = ta.getFloat(R.styleable.SwitchView_offAlpha, 0.2f);
+        onColor = ta.getColor(R.styleable.SwitchView_onColor, 0xFF7BE188);
+        onColorBg = ta.getColor(R.styleable.SwitchView_onColorBg, addAlpha(onColor, onAlpha));
+        offColor = ta.getColor(R.styleable.SwitchView_offColor, 0xFF86909C);
+        offColorBg = ta.getColor(R.styleable.SwitchView_offColorBg, addAlpha(offColor, offAlpha));
+        borderColor = ta.getColor(R.styleable.SwitchView_borderColor, 0xFFE5E6EB);
+        isChecked = ta.getBoolean(R.styleable.SwitchView_isChecked, false);
+        ta.recycle();
+
+        // 初始化画笔
+        switchPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        switchPaint.setAntiAlias(true);
+
+        borderPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        borderPaint.setAntiAlias(true);
+        borderPaint.setStyle(Paint.Style.STROKE);
+        borderPaint.setStrokeWidth(borderWidth);
+
+        // 初始化裁剪路径和边框路径
+        clipPath = new Path();
+        borderPath = new Path();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        setMeasuredDimension(switchWidth, switchHeight);
+        // 计算圆形的半径，这里使用switchHeight的一半作为圆形的半径
+        radius = switchHeight / 2;
+        // 设置边框矩形
+        borderRectF = new RectF(0, 0, switchWidth, switchHeight);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // 创建裁剪区域，使得绘制的内容只在边框内部显示
+        clipPath.reset();
+        clipPath.addRoundRect(borderRectF, radius, radius, Path.Direction.CW);
+        canvas.clipPath(clipPath);
+
+        // 根据isChecked设置背景颜色（只在边框内部绘制）
+        int backgroundColor = isChecked ? onColorBg : offColorBg;
+        canvas.drawColor(backgroundColor);
+
+        // 绘制开或关对应的圆形
+        int centerX = isChecked ? switchWidth - radius : radius;
+        int centerY = switchHeight / 2;
+
+        // 绘制圆形
+        int circleColor = isChecked ? onColor : offColor;
+        switchPaint.setColor(circleColor);
+        switchPaint.setStyle(Paint.Style.FILL);
+        canvas.drawCircle(centerX, centerY, radius, switchPaint);
+
+        // 绘制边框
+        borderPath.reset();
+        borderPath.addRoundRect(borderRectF, radius, radius, Path.Direction.CW);
+        borderPaint.setColor(borderColor); // 设置边框颜色
+        canvas.drawPath(borderPath, borderPaint);
+    }
+
+
+    // 添加透明度，返回一个带有透明度的新颜色值。颜色的透明度范围是 0（完全透明）到 255（完全不透明）
+    public int addAlpha(int color, float alpha) {
+        int alphaValue = (int) (alpha * 255) << 24;
+        return (color & 0x00FFFFFF) | alphaValue;
+    }
+
+    public void setClickListener(ClickListener listener) {
+        this.listener = listener;
+    }
+
+    public void setChecked(boolean checked) {
+        isChecked = checked;
+        invalidate();
+        if (listener != null) {
+            listener.onSwitchStateChanged(isChecked);
+        }
+    }
+
+    public boolean isChecked() {
+        return isChecked;
+    }
+
+    public interface ClickListener {
+        void onSwitchStateChanged(boolean isChecked);
+    }
+}
--- app/src/main/java/com/wxdgut/commonui/test/TestActivity.java
+++ app/src/main/java/com/wxdgut/commonui/test/TestActivity.java
@@ -6,6 +6,7 @@ import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -13,6 +14,7 @@ import android.widget.TextView;
 
 import com.wxdgut.commonui.R;
 import com.wxdgut.uilibrary.iv.ImageViewPro;
+import com.wxdgut.uilibrary.switchview.SwitchView;
 
 public class TestActivity extends BaseTestActivity implements View.OnClickListener {
     //视图控件
@@ -28,6 +30,13 @@ public class TestActivity extends BaseTestActivity implements View.OnClickListen
         setContentView(R.layout.activity_test);
         initView();
         initEvent();
+        SwitchView sw3 = findViewById(R.id.sw3);
+        sw3.setClickListener(new SwitchView.ClickListener() {
+            @Override
+            public void onSwitchStateChanged(boolean isChecked) {
+                Log.i("SwitchView", "onSwitchStateChanged: " + isChecked);
+            }
+        });
     }
 
     //初始化视图控件
